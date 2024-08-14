887a75e119a5b9dd46da826246306ba4a075f260
Balhash-old <tel-alex@mail.ru>
2024-08-14 09:36:43 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+my_snring=input('Введите текст:  ')
+print(my_snring .upper())
+print(my_snring.lower())
+print(my_snring.replace(' '  ,'#'))
+print(my_snring .replace(' ',''))
+print('Количество знаков в строке', my_snring, '17')
+print('Первый символ строки',my_snring , ' -П')
+print('Последний символ строки',my_snring, ' -г')
